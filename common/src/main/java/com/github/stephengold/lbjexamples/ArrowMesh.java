07e9424361a8a4284eadaa1ec2d77b3079479275
Stephen Gold <sgold@sonic.net>
2022-05-19 08:10:33 -0700

    ArrowMesh:  add the getMesh() method to encourage re-use of meshes

--- common/src/main/java/com/github/stephengold/lbjexamples/ArrowMesh.java
+++ common/src/main/java/com/github/stephengold/lbjexamples/ArrowMesh.java
@@ -30,7 +30,9 @@
 package com.github.stephengold.lbjexamples;
 
 import com.jme3.math.FastMath;
+import com.jme3.math.Transform;
 import jme3utilities.Validate;
+import jme3utilities.math.MyVector3f;
 import org.lwjgl.opengl.GL11;
 
 /**
@@ -39,6 +41,21 @@ import org.lwjgl.opengl.GL11;
  * @author Stephen Gold sgold@sonic.net
  */
 public class ArrowMesh extends Mesh {
+    // *************************************************************************
+    // fields
+
+    /**
+     * arrow with its tip at (1, 0, 0) and its tail at (0, 0, 0)
+     */
+    private static ArrowMesh xArrow;
+    /**
+     * arrow with its tip at (0, 1, 0) and its tail at (0, 0, 0)
+     */
+    private static ArrowMesh yArrow;
+    /**
+     * arrow with its tip at (0, 0, 1) and its tail at (0, 0, 0)
+     */
+    private static ArrowMesh zArrow;
     // *************************************************************************
     // constructors
 
@@ -75,5 +92,46 @@ public class ArrowMesh extends Mesh {
                 0f, 0f, 1f, // tip
                 0f, -xy, z, // -Y barb
                 0f, 0f, 1f); // tip
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the mesh to represent the specified axis.
+     *
+     * @param axisIndex which axis: 0&rarr;X, 1&rarr;Y, 2&rarr;Z
+     */
+    public static ArrowMesh getMesh(int axisIndex) {
+        switch (axisIndex) {
+            case MyVector3f.xAxis:
+                if (xArrow == null) {
+                    Transform rotZ2X = new Transform();
+                    rotZ2X.getRotation()
+                            .fromAngles(0f, FastMath.HALF_PI, 0f);
+                    xArrow = new ArrowMesh();
+                    xArrow.transform(rotZ2X);
+                }
+                return xArrow;
+
+            case MyVector3f.yAxis:
+                if (yArrow == null) {
+                    Transform rotZ2Y = new Transform();
+                    rotZ2Y.getRotation()
+                            .fromAngles(-FastMath.HALF_PI, 0f, 0f);
+                    yArrow = new ArrowMesh();
+                    yArrow.transform(rotZ2Y);
+                }
+                return yArrow;
+
+            case MyVector3f.zAxis:
+                if (zArrow == null) {
+                    zArrow = new ArrowMesh();
+                }
+                return zArrow;
+
+            default:
+                String message = "axisIndex = " + axisIndex;
+                throw new IllegalArgumentException(message);
+        }
     }
 }
