3bcac6078b3363233f4ee489993e1a7d4e2c9511
Stephen Gold <sgold@sonic.net>
2022-06-01 20:21:33 -0700

    apps:  move the console apps to a new package

--- apps/src/main/java/com/github/stephengold/lbjexamples/apps/console/HelloVehicle0.java
+++ apps/src/main/java/com/github/stephengold/lbjexamples/apps/console/HelloVehicle0.java
@@ -27,7 +27,7 @@
  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.github.stephengold.lbjexamples.apps;
+package com.github.stephengold.lbjexamples.apps.console;
 
 import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.collision.shapes.CollisionShape;
