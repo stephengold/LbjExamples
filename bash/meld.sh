c6e43fe3d3da6148a220721685e820b9b2ccca7a
Stephen Gold <sgold@sonic.net>
2023-08-11 15:16:33 -0700

    meld.sh:  also compare the test package with V-Sport

--- bash/meld.sh
+++ bash/meld.sh
@@ -13,4 +13,7 @@ D2=$GitDir/V-Sport/lib/src/main/java/com/github/stephengold/vsport
 S3=$GitDir/LbjExamples/
 D3=$GitDir/V-Sport/
 
-/usr/bin/meld --diff $S1 $D1 --diff $S2 $D2 --diff $S3 $D3
+S4=$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/sport/test
+D4=$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/test
+
+/usr/bin/meld --diff $S1 $D1 --diff $S2 $D2 --diff $S3 $D3 --diff $S4 $D4
