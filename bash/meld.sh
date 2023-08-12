72d3662e354626b9acb8592b232996451cd53af7
Stephen Gold <sgold@sonic.net>
2023-08-12 08:21:19 -0700

    meld.sh:  quote the filesystem paths

--- bash/meld.sh
+++ bash/meld.sh
@@ -4,16 +4,16 @@ set -e
 
 GitDir=~/NetBeansProjects
 
-S1=$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples/apps
-D1=$GitDir/Minie/TutorialApps/src/main/java/jme3utilities/tutorial
+S1="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples/apps"
+D1="$GitDir/Minie/TutorialApps/src/main/java/jme3utilities/tutorial"
 
-S2=$GitDir/LbjExamples/common/src/main/java/com/github/stephengold/sport
-D2=$GitDir/V-Sport/lib/src/main/java/com/github/stephengold/vsport
+S2="$GitDir/LbjExamples/common/src/main/java/com/github/stephengold/sport"
+D2="$GitDir/V-Sport/lib/src/main/java/com/github/stephengold/vsport"
 
-S3=$GitDir/LbjExamples/
-D3=$GitDir/V-Sport/
+S3="$GitDir/LbjExamples/"
+D3="$GitDir/V-Sport/"
 
-S4=$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/sport/test
-D4=$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/test
+S4="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/sport/test"
+D4="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/test"
 
-/usr/bin/meld --diff $S1 $D1 --diff $S2 $D2 --diff $S3 $D3 --diff $S4 $D4
+/usr/bin/meld --diff "$S1" "$D1" --diff "$S2" "$D2" --diff "$S3" "$D3" --diff "$S4" "$D4"
