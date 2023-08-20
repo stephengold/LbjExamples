181a04c096bb7fda1c2b4c91aba7b6cf1f4d8419
Stephen Gold <sgold@sonic.net>
2023-08-19 20:12:22 -0700

    Projection:  add public constants "farClipZ" and "nearClipZ"

--- common/src/main/java/com/github/stephengold/sport/Projection.java
+++ common/src/main/java/com/github/stephengold/sport/Projection.java
@@ -65,6 +65,17 @@ import org.joml.Vector4f;
  */
 public class Projection extends GlobalUniform {
     // *************************************************************************
+    // constants
+
+    /**
+     * Z coordinate of the far plane in clipspace
+     */
+    final public static float farClipZ = 1f;
+    /**
+     * Z coordinate of the near plane in clipspace
+     */
+    final public static float nearClipZ = -1f;
+    // *************************************************************************
     // fields
 
     /**
