ecb7634e7d7f775b48d317237e58629a450d8663
Stephen Gold <sgold@sonic.net>
2023-09-03 13:43:36 -0700

    meld.sh:  add 2 more diffs

--- bash/meld.sh
+++ bash/meld.sh
@@ -17,7 +17,13 @@ D3="$GitDir/V-Sport/"
 S4="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/sport/test"
 D4="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/test"
 
+S5="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples"
+D5="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/demo"
+
+S6="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples/apps"
+D6="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/tutorial"
+
 Meld="/usr/bin/meld"
 #Meld="/c/Program Files/Meld/meld"
 
-"$Meld" --diff "$S1" "$D1" --diff "$S2" "$D2" --diff "$S3" "$D3" --diff "$S4" "$D4"
+"$Meld" --diff "$S1" "$D1" --diff "$S2" "$D2" --diff "$S3" "$D3" --diff "$S4" "$D4" --diff "$S5" "$D5" --diff "$S6" "$D6"
