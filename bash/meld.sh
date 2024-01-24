a65f6fccde2e2c7fb955d1e47ef722e52be134ef
Stephen Gold <sgold@sonic.net>
2024-01-24 14:52:05 -0800

    meld.sh:  add another comparison view

--- bash/meld.sh
+++ bash/meld.sh
@@ -23,7 +23,10 @@ D5="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/demo"
 S6="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples/apps"
 D6="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/tutorial"
 
+S7="$GitDir/LbjExamples/apps/src/main/java"
+D7="$GitDir/Minie/MinieExamples/src/main/java"
+
 Meld="/usr/bin/meld"
 #Meld="/c/Program Files/Meld/meld"
 
-"$Meld" --diff "$S1" "$D1" --diff "$S2" "$D2" --diff "$S3" "$D3" --diff "$S4" "$D4" --diff "$S5" "$D5" --diff "$S6" "$D6"
+"$Meld" --diff "$S1" "$D1" --diff "$S2" "$D2" --diff "$S3" "$D3" --diff "$S4" "$D4" --diff "$S5" "$D5" --diff "$S6" "$D6" --diff "$S7" "$D7"
