9815c0485cd622889601fa26ff783d5595a79e8a
stephengold <sgold@sonic.net>
2024-05-11 14:29:30 -0700

    meld.sh: compare the Antora documentation with the Minie project

--- bash/meld.sh
+++ bash/meld.sh
@@ -11,6 +11,12 @@ D1="$GitDir/Minie/TutorialApps/src/main/java/jme3utilities/tutorial"
 S2="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples"
 D2="$GitDir/sport/apps/src/main/java/com/github/stephengold/sport/demo"
 
+S3="$GitDir/LbjExamples/docs/en/modules/ROOT"
+D3="$GitDir/Minie/MinieLibrary/src/site/antora/tutorials/modules/minie-library-tutorials"
+
+S4="$GitDir/LbjExamples/docs/en/modules/ROOT"
+D4="$GitDir/Minie/src/site/antora/minie-project/modules/ROOT"
+
 S5="$GitDir/LbjExamples/apps/src/main/java/com/github/stephengold/lbjexamples"
 D5="$GitDir/V-Sport/apps/src/main/java/com/github/stephengold/vsport/demo"
 
@@ -23,4 +29,5 @@ D7="$GitDir/Minie/MinieExamples/src/main/java"
 Meld="/usr/bin/meld"
 #Meld="/c/Program Files/Meld/meld"
 
-"$Meld" --diff "$S1" "$D1" --diff "$S2" "$D2" --diff "$S5" "$D5" --diff "$S6" "$D6" --diff "$S7" "$D7"
+"$Meld" --diff "$S1" "$D1" --diff "$S2" "$D2" --diff "$S3" "$D3" --diff "$S4" "$D4" \
+        --diff "$S5" "$D5" --diff "$S6" "$D6" --diff "$S7" "$D7"
