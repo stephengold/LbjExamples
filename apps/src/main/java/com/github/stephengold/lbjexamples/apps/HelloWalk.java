07c24ba5124e668538ad8f5cd8f9cdd010535bf3
stephengold <sgold@sonic.net>
2025-06-05 20:35:41 -0700

    HelloWalk:  re-order methods

--- apps/src/main/java/com/github/stephengold/lbjexamples/apps/HelloWalk.java
+++ apps/src/main/java/com/github/stephengold/lbjexamples/apps/HelloWalk.java
@@ -150,6 +150,18 @@ public class HelloWalk
     // PhysicsTickListener methods
 
     /**
+     * Callback from Bullet, invoked just after each simulation step.
+     *
+     * @param space the space that was just stepped (not null)
+     * @param timeStep the duration of the simulation step (in seconds, &ge;0)
+     */
+    @Override
+    public void physicsTick(PhysicsSpace space, float timeStep) {
+        Vector3f location = character.getPhysicsLocation(null);
+        cam.setLocation(location);
+    }
+
+    /**
      * Callback from Bullet, invoked just before each simulation step.
      *
      * @param space the space that's about to be stepped (not null)
@@ -175,18 +187,6 @@ public class HelloWalk
                 character.setWalkDirection(offset);
             }
         }
-    }
-
-    /**
-     * Callback from Bullet, invoked just after each simulation step.
-     *
-     * @param space the space that was just stepped (not null)
-     * @param timeStep the duration of the simulation step (in seconds, &ge;0)
-     */
-    @Override
-    public void physicsTick(PhysicsSpace space, float timeStep) {
-        Vector3f location = character.getPhysicsLocation(null);
-        cam.setLocation(location);
     }
     // *************************************************************************
     // private methods
