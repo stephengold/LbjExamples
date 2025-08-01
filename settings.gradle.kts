89ef2da05c7a0d5e64cd07fcf4a5471408764b8e
stephengold <sgold@sonic.net>
2025-07-31 17:55:06 -0700

    obtain snapshot artifacts from the Central Portal

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,10 @@ dependencyResolutionManagement {
     repositories {
         //mavenLocal() // to find libraries installed locally
         mavenCentral() // to find libraries released to the Maven Central repository
-        //maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") } // to find public snapshots of LWJGL
-        //maven { url = uri("https://s01.oss.sonatype.org/content/groups/staging") } // to find libraries staged but not yet released
-        //maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots") } // to find public snapshots of libraries
+        maven {
+            name = "Central Portal Snapshots"
+            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
+        }
     }
 }
 
